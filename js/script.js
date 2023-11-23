011f362ebc184ca6c831cee7d3f64fe4c3e304a8
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-23 17:01:51 +0100

    add: struttura carosello

--- js/script.js
+++ js/script.js
@@ -1,1 +1,109 @@
 
+// creiamo le costanti
+
+const items = document.getElementsByClassName('items');
+
+console.log(items);
+
+let activeItem = 0;
+
+// navigazione
+
+const next = document.getElementById ('buttonnext');
+const prev = document.getElementById ('buttonprev');
+
+
+next.addEventListener ('click',
+
+ function (){
+
+    if (activeItem < items.length -1){
+
+        // togliamo la classe block all'elemento corrente
+
+        items [activeItem].classList.remove ('block');
+
+        // aggiungiamo di uno l'indice da visualizzare
+
+        activeItem++;
+         console.log(activeItem);
+        // aggiungiamo la classe bloch all'item successivo
+
+        items [activeItem].classList.add ('block');
+
+
+        if(activeItem === items.length -1){
+
+            items [activeItem].classList.remove ('block');
+
+            activeItem = 0;
+
+            items [0].classList.add ('block');
+            console.log(items [0]);
+        }
+
+    
+    }
+ }
+);
+
+
+
+prev.addEventListener ('click',
+
+ function (){
+
+    if (activeItem < items.length -1){
+
+        // togliamo la classe block all'elemento corrente
+
+        items [activeItem].classList.remove ('block');
+
+        // aggiungiamo di uno l'indice da visualizzare
+
+        activeItem--;
+        console.log(activeItem);
+
+        // aggiungiamo la classe bloch all'item successivo
+
+        items [activeItem].classList.add ('block');
+
+
+        if(activeItem === items [0]){
+
+            items [activeItem].classList.remove ('block');
+            console.log(activeItem);
+
+            activeItem = 4;
+
+            items [activeItem].classList.add ('block');
+            
+            console.log(activeItem);
+        }
+
+    
+    }
+ }
+);
+
+
+
+// next.addEventListener ('click',
+//    function(){
+
+  
+//      for(let i = 0; i <= items.length -1; i++){
+        
+
+//         items [i].classList.remove ('block');
+//             console.log(items [i]);
+
+        
+
+//         items [i++].classList.add ('block');
+         
+//         console.log(items [i]);
+
+// }
+
+// });
